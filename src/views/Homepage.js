1b1ff08b5e53dd05e691339aaea6c1c8d4cc01b1
Junio Caesar <junio.caesar@gmail.com>
2023-03-11 01:48:25 +0700

    Update View

---
+++ src/views/Homepage.js
@@ -0,0 +1,199 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import MenuItem from "@mui/material/MenuItem";
+import backgroundImage from '../img/background.png';
+const pages = ["Form Pendaftaran"];
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
+
+function ResponsiveAppBar() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <Box>
+      <AppBar sx={{ background: "#003177" }}>
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            {/* <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} /> */}
+            <Box sx={{ mr: 2, display: { xs: "none", md: "block" } }}>
+              <img src={require("../img/logo.png")} width="40" />
+            </Box>
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="/"
+              sx={{
+                mr: 2,
+                display: { xs: "none", md: "flex" },
+                fontFamily: "Roboto",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              YOUTH GPDI METRO
+            </Typography>
+            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: "block", md: "none" },
+                }}
+              >
+                {pages.map((page) => (
+                  <MenuItem key={page} onClick={handleCloseNavMenu}>
+                    <Typography textAlign="center">{page}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+            {/* <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} /> */}
+            <Box
+              sx={{ flexGrow: 1, mr: 2, display: { xs: "flex", md: "none" } }}
+            >
+              <img src={require("../img/logo.png")} width="40" />
+            </Box>
+            {/* <Typography
+              variant="h5"
+              noWrap
+              component="a"
+              href=""
+              sx={{
+                  mr: 2,
+                  display: { xs: "flex", md: "none" },
+                  flexGrow: 1,
+                  fontFamily: "monospace",
+                  fontWeight: 700,
+                  letterSpacing: ".3rem",
+                  color: "inherit",
+                  textDecoration: "none",
+                }}
+            >
+              LOGO
+            </Typography> */}
+            <Box
+              sx={{ flexGrow: 1, display: { xs: "none", md: "block" } }}
+            ></Box>
+            <Box sx={{ flexGrow: 0, display: { xs: "none", md: "flex" } }}>
+              {pages.map((page) => (
+                <Button
+                  key={page}
+                  onClick={handleCloseNavMenu}
+                  sx={{ my: 2, color: "white", display: "block" }}
+                >
+                  {page}
+                </Button>
+              ))}
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+
+      <Box
+        style={{
+          backgroundImage: `url(${backgroundImage})`,
+          backgroundSize: "cover",
+          height: "100vh",
+        }}
+      >
+
+      <Box
+        component="main"
+        sx={{
+          p: 3,
+          minHeight: "100vh",
+        }}
+      >
+        <Toolbar />
+        <Typography>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+          unde fugit veniam eius, perspiciatis sunt? Corporis qui ducimus
+          quibusdam, aliquam dolore excepturi quae. Distinctio enim at eligendi
+          perferendis in cum quibusdam sed quae, accusantium et aperiam? Quod
+          itaque exercitationem, at ab sequi qui modi delectus quia corrupti
+          alias distinctio nostrum. Minima ex dolor modi inventore sapiente
+          necessitatibus aliquam fuga et. Sed numquam quibusdam at officia
+          sapiente porro maxime corrupti perspiciatis asperiores, exercitationem
+          eius nostrum consequuntur iure aliquam itaque, assumenda et! Quibusdam
+          temporibus beatae doloremque voluptatum doloribus soluta accusamus
+          porro reprehenderit eos inventore facere, fugit, molestiae ab officiis
+          illo voluptates recusandae. Vel dolor nobis eius, ratione atque
+          soluta, aliquam fugit qui iste architecto perspiciatis. Nobis,
+          voluptatem! Cumque, eligendi unde aliquid minus quis sit debitis
+          obcaecati error, delectus quo eius exercitationem tempore. Delectus
+          sapiente, provident corporis dolorum quibusdam aut beatae repellendus
+          est labore quisquam praesentium repudiandae non vel laboriosam quo ab
+          perferendis velit ipsa deleniti modi! Ipsam, illo quod. Nesciunt
+          commodi nihil corrupti cum non fugiat praesentium doloremque
+          architecto laborum aliquid. Quae, maxime recusandae? Eveniet dolore
+          molestiae dicta blanditiis est expedita eius debitis cupiditate porro
+          sed aspernatur quidem, repellat nihil quasi praesentium quia eos,
+          quibusdam provident. Incidunt tempore vel placeat voluptate iure
+          labore, repellendus beatae quia unde est aliquid dolor molestias
+          libero. Reiciendis similique exercitationem consequatur, nobis placeat
+          illo laudantium! Enim perferendis nulla soluta magni error, provident
+          repellat similique cupiditate ipsam, et tempore cumque quod! Qui, iure
+          suscipit tempora unde rerum autem saepe nisi vel cupiditate iusto.
+          Illum, corrupti? Fugiat quidem accusantium nulla. Aliquid inventore
+          commodi reprehenderit rerum reiciendis! Quidem alias repudiandae eaque
+          eveniet cumque nihil aliquam in expedita, impedit quas ipsum nesciunt
+          ipsa ullam consequuntur dignissimos numquam at nisi porro a, quaerat
+          rem repellendus. Voluptates perspiciatis, in pariatur impedit, nam
+          facilis libero dolorem dolores sunt inventore perferendis, aut
+          sapiente modi nesciunt.
+        </Typography>
+      </Box>
+
+      </Box>
+      <Box sx={{ flexGrow: 1, background: "#003177", height: "100px" }}></Box>
+    </Box>
+  );
+}
+export default ResponsiveAppBar;
